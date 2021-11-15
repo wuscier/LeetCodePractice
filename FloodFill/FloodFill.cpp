c66a5441e68ea9ce263d3c317814adb7b7db757f
wuscier <1352288123@qq.com>
2021-11-15 22:42:14 +0800

    flood fill

--- FloodFill/FloodFill.cpp
+++ FloodFill/FloodFill.cpp
@@ -7,20 +7,81 @@
 
 using namespace std;
 
-//class Solution {
-//public:
-//    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-//
-//    }
-//};
-//
-//
-//int main()
-//{
-//    Solution solution;
-//    solution.floodFill();
-//    std::cout << "Hello World!\n";
-//}
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+
+        if (image.size() == 0)
+        {
+            return image;
+        }
+
+        int val = image[sr][sc];
+
+        if (val == newColor) {
+            return image;
+        }
+
+        int width = image[0].size();
+        int height = image.size();
+
+        queue<pair<int, int>> q;
+
+        q.push(make_pair(sr, sc));
+
+        while (!q.empty())
+        {
+            int size = q.size();
+
+            for (size_t i = 0; i < size; i++)
+            {
+                pair<int, int> point = q.front();
+                q.pop();
+
+                // 左
+                if (point.first - 1 >= 0 && image[point.first - 1][point.second] == val)
+                {
+                    q.push(make_pair(point.first - 1, point.second));
+                }
+
+                // 上
+                if (point.second - 1 >= 0 && image[point.first][point.second - 1] == val)
+                {
+                    q.push(make_pair(point.first, point.second - 1));
+                }
+
+
+                // 右
+                if ((point.first + 1 < height) && (image[point.first + 1][point.second] == val))
+                {
+                    q.push(make_pair(point.first + 1, point.second));
+                }
+
+
+                // 下
+                if (point.second + 1 < width && image[point.first][point.second + 1] == val)
+                {
+                    q.push(make_pair(point.first, point.second + 1));
+                }
+
+                image[point.first][point.second] = newColor;
+
+            }
+        }
+
+        return image;
+    }
+};
+
+int main()
+{
+    //vector<vector<int>> v = { {1,1,1},{1,1,0},{1,0,1} };
+    vector<vector<int>> v = { {0,0,0},{0,1,1} };
+
+    Solution solution;
+    solution.floodFill(v, 1, 1, 1);
+    std::cout << "Hello World!\n";
+}
 
 
 
@@ -33,66 +94,66 @@ using namespace std;
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
  
-class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> vals;
-
-        if (root == nullptr)
-        {
-            return vals;
-        }
-
-        queue<TreeNode*> nodes;
-        nodes.push(root);
-
-        while (!nodes.empty())
-        {
-            int size = nodes.size();
-            vals.push_back(vector<int>());
-
-            for (size_t i = 0; i < size; i++)
-            {
-                TreeNode* node = nodes.front();
-                nodes.pop();
-
-                if (node != nullptr)
-                {
-                    vals.back().push_back(node->val);
-
-                    if (node->left != nullptr)
-                    {
-                        nodes.push(node->left);
-                    }
-
-                    if (node->right != nullptr)
-                    {
-                        nodes.push(node->right);
-                    }
-                }
-            }
-        }
-
-        return vals;
-    }
-};
-
-
-int main() {
-    TreeNode* fifteen = new TreeNode(15);
-    TreeNode* seven = new TreeNode(7);
-
-    TreeNode* nine = new TreeNode(9);
-    TreeNode* twenty = new TreeNode(20, fifteen, seven);
-
-    TreeNode* root = new TreeNode(3, nine, twenty);
+//class Solution {
+//public:
+//    vector<vector<int>> levelOrder(TreeNode* root) {
+//        vector<vector<int>> vals;
+//
+//        if (root == nullptr)
+//        {
+//            return vals;
+//        }
+//
+//        queue<TreeNode*> nodes;
+//        nodes.push(root);
+//
+//        while (!nodes.empty())
+//        {
+//            int size = nodes.size();
+//            vals.push_back(vector<int>());
+//
+//            for (size_t i = 0; i < size; i++)
+//            {
+//                TreeNode* node = nodes.front();
+//                nodes.pop();
+//
+//                if (node != nullptr)
+//                {
+//                    vals.back().push_back(node->val);
+//
+//                    if (node->left != nullptr)
+//                    {
+//                        nodes.push(node->left);
+//                    }
+//
+//                    if (node->right != nullptr)
+//                    {
+//                        nodes.push(node->right);
+//                    }
+//                }
+//            }
+//        }
+//
+//        return vals;
+//    }
+//};
 
 
-
-
-    Solution solution;
-    solution.levelOrder(root);
-}
+//int main() {
+//    TreeNode* fifteen = new TreeNode(15);
+//    TreeNode* seven = new TreeNode(7);
+//
+//    TreeNode* nine = new TreeNode(9);
+//    TreeNode* twenty = new TreeNode(20, fifteen, seven);
+//
+//    TreeNode* root = new TreeNode(3, nine, twenty);
+//
+//
+//
+//
+//    Solution solution;
+//    solution.levelOrder(root);
+//}
 
 
 
